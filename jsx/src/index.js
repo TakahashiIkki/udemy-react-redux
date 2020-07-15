a79f14632c04530d385827b6c844be3929a3fb30
Takashi Ikki <takahashi@omicale.com>
2020-07-15 14:03:34 +0900

    labelText

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,20 +2,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function getButtonText() {
-    return 'Click On Me!'
-}
-
 // Create a react component
 const App = () => {
     const buttonText = 'Click Me!';
+    const labelText = 'Enter name:';
 
     return (
         <div>
-            <label className="label" htmlFor="name">Enter name:</label>
+            <label className="label" htmlFor="name">
+                {labelText}
+            </label>
             <input id="name" type="text"/>
             <button style={{backgroundColor: 'blue', color: 'white'}}>
-                {getButtonText()}
+                {buttonText}
             </button>
         </div>
     )
