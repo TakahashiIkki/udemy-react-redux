1ed58ac7c2ab8767c3b0e83448dfb54f3835682b
Takashi Ikki <takahashi@omicale.com>
2020-07-15 14:01:02 +0900

    add HTML

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,14 +2,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText() {
+    return 'Click On Me!'
+}
+
 // Create a react component
 const App = () => {
-    return <div>Hi There!</div>
+    const buttonText = 'Click Me!';
+
+    return (
+        <div>
+            <label className="label" htmlFor="name">Enter name:</label>
+            <input id="name" type="text"/>
+            <button style={{backgroundColor: 'blue', color: 'white'}}>
+                {getButtonText()}
+            </button>
+        </div>
+    )
 };
 
 
 // Take the react component and show it on screen
 ReactDOM.render(
-    <App />,
+    <App/>,
     document.querySelector('#root')
 )
