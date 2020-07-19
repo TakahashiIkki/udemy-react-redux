10104f1582ff3960691f2a4e9eac7cdc5a02334b
Takashi Ikki <takahashi@omicale.com>
2020-07-19 22:39:08 +0900

    add content and avatar

--- components/src/index.js
+++ components/src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author={faker.name.firstName()}/>
-            <CommentDetail author="Sam"/>
-            <CommentDetail author={faker.name.firstName()}/>
+            <CommentDetail
+                author={faker.name.firstName()}
+                timeAgo="Today at 4:45PM"
+                content="Nice Blog Post"
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author="Sam"
+                timeAgo="Today at 2:00AM"
+                content="I like the subject"
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author={faker.name.firstName()}
+                timeAgo="Yesterday at 5:00PM"
+                content="I like the writing"
+                avatar={faker.image.avatar()}
+            />
         </div>
     )
 };
