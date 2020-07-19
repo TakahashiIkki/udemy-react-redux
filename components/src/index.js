998945ab8f30882d851dd75bfd5472581e15f45a
Takashi Ikki <takahashi@omicale.com>
2020-07-19 22:49:58 +0900

    子コンポーネントをimportできるように

--- components/src/index.js
+++ components/src/index.js
@@ -2,28 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail
-                author={faker.name.firstName()}
-                timeAgo="Today at 4:45PM"
-                content="Nice Blog Post"
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail
-                author="Sam"
-                timeAgo="Today at 2:00AM"
-                content="I like the subject"
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail
-                author={faker.name.firstName()}
-                timeAgo="Yesterday at 5:00PM"
-                content="I like the writing"
-                avatar={faker.image.avatar()}
-            />
+            <ApprovalCard>
+                <CommentDetail
+                    author={faker.name.firstName()}
+                    timeAgo="Today at 4:45PM"
+                    content="Nice Blog Post"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sam"
+                    timeAgo="Today at 2:00AM"
+                    content="I like the subject"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author={faker.name.firstName()}
+                    timeAgo="Yesterday at 5:00PM"
+                    content="I like the writing"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
         </div>
     )
 };
