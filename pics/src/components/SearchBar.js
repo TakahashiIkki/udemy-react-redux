2632f2c57a40b14081c358df8ec22b8f98386b96
Takashi Ikki <takahashi@omicale.com>
2020-08-21 13:53:02 +0900

    onFormSubmit

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -3,10 +3,17 @@ import React from "react";
 class SearchBar extends React.Component {
     state = {term: ''};
 
+    onFormSubmit = event => {
+        // 実行したイベントがキャンセル可能ならキャンセルする
+        event.preventDefault();
+
+        console.log(this.state.term);
+    }
+
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
                         <input type="text"
